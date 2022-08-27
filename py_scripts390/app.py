c2454ce53ed04b5da88a641dcc127d116f9850ee
Deepak Mane <deepak.mane123@gmail.com>
2022-08-26 22:46:29 -0700

    refactoring ship

--- py_scripts390/app.py
+++ py_scripts390/app.py
@@ -11,6 +11,9 @@ clock = pygame.time.Clock()
 
 # ship import
 ship_surf = pygame.image.load('./graphics/ship.png').convert_alpha()
+ship_surf_rotated = pygame.transform.rotate(ship_surf, 45)
+ship_surf_scaled = pygame.transform.scale(ship_surf, (600, 50))
+ship_reversed_surf = pygame.transform.flip(ship_surf, False, True)
 ship_rect = ship_surf.get_rect(center=(WINOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 # importing images & background
@@ -24,11 +27,13 @@ laser_rect = laser_surf.get_rect(midbottom=ship_rect.midtop)
 font = pygame.font.Font('./graphics/subatomic.ttf', 50)
 # White can be replace with tuple (255,255,255)
 text_surf = font.render('Space', True, 'White')
-
 # create a surface
 test_surf = pygame.Surface((400, 100))
 # Need to attach surface to display surface. Both of surfaces are black as well
 text_rect = text_surf.get_rect(midbottom=(WINOW_WIDTH / 2, WINDOW_HEIGHT - 80))
+
+# drawing
+test_rect = pygame.Rect(100, 200, 400, 500)
 
 while True:
     # run forever => keeps our game running
@@ -46,9 +51,19 @@ while True:
     # drawing
     display_surface.fill((0, 0, 0))
     display_surface.blit(bg_surf, (0, 0))
-    display_surface.blit(ship_surf, ship_rect)
-    display_surface.blit(text_surf, text_rect)
-    display_surface.blit(laser_surf, laser_rect)\
 
+    display_surface.blit(text_surf, text_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # rect drawing
+    # pygame.draw.rect(display_surface, 'purple',
+    #                  test_rect, width=10, border_radius=5)
+    # pygame.draw.lines(display_surface, 'red', False, [
+    #                   (0, 0), (200, 0), (300, 100)], width=1)
+    pygame.draw.rect(display_surface, (255, 255, 255),
+                     text_rect.inflate(30, 30), width=8, border_radius=5)
+    # display_surface.blit(ship_reversed_surf, ship_rect)
+    # display_surface.blit(ship_surf_scaled, ship_rect)
+    display_surface.blit(ship_surf_rotated, ship_rect)
     # draw the final frame
     pygame.display.update()
